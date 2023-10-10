b50089f697bec876386da1523f860c2c37c033e8
YangMr <759168442@qq.com>
2023-10-10 16:38:07 +0800

    feta(task):实现待提货

--- pages/task/index.vue
+++ pages/task/index.vue
@@ -1,7 +1,51 @@
+<script setup>
+import { ref } from 'vue';
+
+import Pickup from './components/pickup.vue';
+import Delivery from './components/delivery.vue';
+import Complete from './components/complete.vue';
+
+// 控制tab的选中
+const tabIndex = ref(0);
+
+// tab的数据
+const tabMetas = ref([
+	{ title: '待提货', rendered: true },
+	{ title: '在途', rendered: false },
+	{ title: '已完成', rendered: false }
+]);
+
+// 监听tab切花
+const changeTab = (index) => {
+	tabMetas.value[index].rendered = true;
+	tabIndex.value = index;
+};
+</script>
+
 <template>
-	<view>任务</view>
+	<view class="page-container">
+		<view class="tab">
+			<view @click="changeTab(index)" class="tab-item" v-for="(item, index) in tabMetas" :key="index">
+				<view class="title" :class="{ active: tabIndex === index }">
+					{{ item.title }}
+				</view>
+			</view>
+		</view>
+		<view class="message-list" v-show="tabIndex === 0" v-if="tabMetas[0].rendered">
+			<!-- 待提货 -->
+			<Pickup></Pickup>
+		</view>
+		<view class="message-list" v-show="tabIndex === 1" v-if="tabMetas[1].rendered">
+			<!-- 在途 -->
+			<Delivery></Delivery>
+		</view>
+		<view class="message-list" v-show="tabIndex === 2" v-if="tabMetas[2].rendered">
+			<!-- 已完成 -->
+			<Complete></Complete>
+		</view>
+	</view>
 </template>
 
-<script setup></script>
-
-<style lang="scss"></style>
+<style lang="scss">
+@import './index.scss';
+</style>
---
+++ subpkg_task/pickup/index.vue
@@ -0,0 +1,7 @@
+<template>
+	<view>提货信息</view>
+</template>
+
+<script></script>
+
+<style></style>
